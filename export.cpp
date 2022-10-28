5aa7effb2401e5ebf68e067fc1cbf810eda137fa
aliyun7728899324 <12169931@qq.com>
2022-10-28 10:14:39 +0800

    project init

---
+++ export.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include "export.h"
+
+#if !(ENABLE_STATIC_COMPILATION)
+__shared_api_ void* __stdcall api_object_init(const void*, unsigned long) {
+ void* result = nullptr;
+ do {
+  local::__gpLibuv = new local::Libuv();
+  if (!local::__gpLibuv)
+   break;
+  result = local::__gpLibuv;
+ } while (0);
+ return result;
+}
+
+__shared_api_ void __stdcall api_object_uninit() {
+ SK_DELETE_PTR(local::__gpLibuv);
+}
+#endif///#if !(ENABLE_STATIC_COMPILATION)
